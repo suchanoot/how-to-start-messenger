7e98e9a21e501f5e4a11b11b6f7c2ea5ce75bead
suchanoot <far_suchanoot@hotmail.com>
2016-12-11 09:40:09 +0700

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/', function (req, res) {
   res.send('test test')
 })
 app.get('/webhook/', function (req, res) {
-  if (req.query['hub.verify_token'] === 'bot') {
+  if (req.query['hub.verify_token'] === 'botfinal') {
     res.send(req.query['hub.challenge'])
   }
   res.send('Error, wrong token')
