a1565bf2c30755657a589179999b7370880e795d
suchanoot <far_suchanoot@hotmail.com>
2016-12-11 11:31:08 +0700

    Update server.js

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.post('/webhook/', function (req, res) {
         sendGenericMessage(sender)
         continue
       }
-      sendTextMessage(sender, 'พยากรณ์อากาศให้คุณ' + text.substring(0, 200))
+      sendTextMessage(sender, '*** พยากรณ์อากาศให้คุณ ที่ ' + text.substring(0, 200) + ' ****')
     }
     if (event.postback) {
       let text = JSON.stringify(event.postback)
